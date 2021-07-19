0fc3bdc86e219a01a6e2e3ffa11dbea935461078
Hernan <h.d.fiorito@gmail.com>
2021-07-18 23:03:28 -0300

    Agregado código para compilar todo en un exe con auto-py-to-exe

--- TP_Quimica/Imagenes.py
+++ TP_Quimica/Imagenes.py
@@ -1,4 +1,16 @@
 import tkinter as tk
+import os
+import sys
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
 
 #Creacion de ventana
 ventana=tk.Tk()
@@ -7,10 +19,10 @@ ventana=tk.Tk()
 ventana.title("Equilibrio Redox")
 
 #Imagen de fondo para ventana
-img=tk.PhotoImage(file="Imagenes/fondo.png")
+img=tk.PhotoImage(file=resource_path("Imagenes\\fondo.png"))
 lbl=tk.Label(ventana, image=img).pack()
-img_voltimetro=tk.PhotoImage(file="Imagenes/modoVoltimetro.png")
-img_apagado=tk.PhotoImage(file="Imagenes/modoApagado.png")
+img_voltimetro=tk.PhotoImage(file=resource_path("Imagenes\modoVoltimetro.png"))
+img_apagado=tk.PhotoImage(file=resource_path("Imagenes\modoApagado.png"))
 
 #Funciones para insertar imagenes
 def borrar_visor():
@@ -77,44 +89,44 @@ def S_negativo():
     tk.Label(ventana, image=img_S_negativo,bd=0).place(x=553,y=260)
 
 #Imagenes de elementos positivos
-img_H_positivo=tk.PhotoImage(file="Imagenes/Hidrogeno_positivo.png")
-img_Al_positivo=tk.PhotoImage(file="Imagenes/Aluminio_positivo.png")
-img_Cu_positivo=tk.PhotoImage(file="Imagenes/Cobre_positivo.png")
-img_Ag_positivo=tk.PhotoImage(file="Imagenes/Plata_positiva.png")
-img_Zn_positivo=tk.PhotoImage(file="Imagenes/Zinc_positivo.png")
-img_Sn_positivo=tk.PhotoImage(file="Imagenes/Estaño_positivo.png")
-img_Fee_positivo=tk.PhotoImage(file="Imagenes/Hierro++_positivo.png")
-img_Feee_positivo=tk.PhotoImage(file="Imagenes/Hierro+++_positivo.png")
-img_FePt_positivo=tk.PhotoImage(file="Imagenes/HierroPt_positivo.png")
-img_S_positivo=tk.PhotoImage(file="Imagenes/Azufre_positivo.png")
+img_H_positivo=tk.PhotoImage(file=resource_path("Imagenes\Hidrogeno_positivo.png"))
+img_Al_positivo=tk.PhotoImage(file=resource_path("Imagenes\Aluminio_positivo.png"))
+img_Cu_positivo=tk.PhotoImage(file=resource_path("Imagenes\Cobre_positivo.png"))
+img_Ag_positivo=tk.PhotoImage(file=resource_path("Imagenes\Plata_positiva.png"))
+img_Zn_positivo=tk.PhotoImage(file=resource_path("Imagenes\Zinc_positivo.png"))
+img_Sn_positivo=tk.PhotoImage(file=resource_path("Imagenes\Estaño_positivo.png"))
+img_Fee_positivo=tk.PhotoImage(file=resource_path("Imagenes\Hierro++_positivo.png"))
+img_Feee_positivo=tk.PhotoImage(file=resource_path("Imagenes\Hierro+++_positivo.png"))
+img_FePt_positivo=tk.PhotoImage(file=resource_path("Imagenes\HierroPt_positivo.png"))
+img_S_positivo=tk.PhotoImage(file=resource_path("Imagenes\Azufre_positivo.png"))
 
 #Imagenes de elementos negativos
-img_H_negativo=tk.PhotoImage(file="Imagenes/Hidrogeno_negativo.png")
-img_Al_negativo=tk.PhotoImage(file="Imagenes/Aluminio_negativo.png")
-img_Cu_negativo=tk.PhotoImage(file="Imagenes/Cobre_negativo.png")
-img_Ag_negativo=tk.PhotoImage(file="Imagenes/Plata_negativa.png")
-img_Zn_negativo=tk.PhotoImage(file="Imagenes/Zinc_negativo.png")
-img_Sn_negativo=tk.PhotoImage(file="Imagenes/Estaño_negativo.png")
-img_Fee_negativo=tk.PhotoImage(file="Imagenes/Hierro++_negativo.png")
-img_Feee_negativo=tk.PhotoImage(file="Imagenes/Hierro+++_negativo.png")
-img_FePt_negativo=tk.PhotoImage(file="Imagenes/HierroPt_negativo.png")
-img_S_negativo=tk.PhotoImage(file="Imagenes/Azufre_negativo.png")
+img_H_negativo=tk.PhotoImage(file=resource_path("Imagenes\Hidrogeno_negativo.png"))
+img_Al_negativo=tk.PhotoImage(file=resource_path("Imagenes\Aluminio_negativo.png"))
+img_Cu_negativo=tk.PhotoImage(file=resource_path("Imagenes\Cobre_negativo.png"))
+img_Ag_negativo=tk.PhotoImage(file=resource_path("Imagenes\Plata_negativa.png"))
+img_Zn_negativo=tk.PhotoImage(file=resource_path("Imagenes\Zinc_negativo.png"))
+img_Sn_negativo=tk.PhotoImage(file=resource_path("Imagenes\Estaño_negativo.png"))
+img_Fee_negativo=tk.PhotoImage(file=resource_path("Imagenes\Hierro++_negativo.png"))
+img_Feee_negativo=tk.PhotoImage(file=resource_path("Imagenes\Hierro+++_negativo.png"))
+img_FePt_negativo=tk.PhotoImage(file=resource_path("Imagenes\HierroPt_negativo.png"))
+img_S_negativo=tk.PhotoImage(file=resource_path("Imagenes\Azufre_negativo.png"))
 
 #Imagenes para voltajes
-img_visor_vacio=tk.PhotoImage(file="Imagenes/visor_vacio.png")
-img_0=tk.PhotoImage(file="Imagenes/0.png")
-img_1=tk.PhotoImage(file="Imagenes/1.png")
-img_2=tk.PhotoImage(file="Imagenes/2.png")
-img_3=tk.PhotoImage(file="Imagenes/3.png")
-img_4=tk.PhotoImage(file="Imagenes/4.png")
-img_5=tk.PhotoImage(file="Imagenes/5.png")
-img_6=tk.PhotoImage(file="Imagenes/6.png")
-img_7=tk.PhotoImage(file="Imagenes/7.png")
-img_8=tk.PhotoImage(file="Imagenes/8.png")
-img_9=tk.PhotoImage(file="Imagenes/9.png")
-img_menos=tk.PhotoImage(file="Imagenes/-.png")
-img_punto=tk.PhotoImage(file="Imagenes/punto.png")
-img_vacio=tk.PhotoImage(file="Imagenes/vacio.png")
+img_visor_vacio=tk.PhotoImage(file=resource_path("Imagenes\\visor_vacio.png"))
+img_0=tk.PhotoImage(file=resource_path("Imagenes\\0.png"))
+img_1=tk.PhotoImage(file=resource_path("Imagenes\\1.png"))
+img_2=tk.PhotoImage(file=resource_path("Imagenes\\2.png"))
+img_3=tk.PhotoImage(file=resource_path("Imagenes\\3.png"))
+img_4=tk.PhotoImage(file=resource_path("Imagenes\\4.png"))
+img_5=tk.PhotoImage(file=resource_path("Imagenes\\5.png"))
+img_6=tk.PhotoImage(file=resource_path("Imagenes\\6.png"))
+img_7=tk.PhotoImage(file=resource_path("Imagenes\\7.png"))
+img_8=tk.PhotoImage(file=resource_path("Imagenes\\8.png"))
+img_9=tk.PhotoImage(file=resource_path("Imagenes\\9.png"))
+img_menos=tk.PhotoImage(file=resource_path("Imagenes\-.png"))
+img_punto=tk.PhotoImage(file=resource_path("Imagenes\punto.png"))
+img_vacio=tk.PhotoImage(file=resource_path("Imagenes\\vacio.png"))
 
 #Diccionario de mapeo de digitos
 diccDigitos = {
